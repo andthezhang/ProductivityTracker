c9eb483188f2074eebe7c758097f1d6438dd90d9
Zhiheng(Andy) Zhang <hiandyzhang@outlook.com>
2020-08-13 16:30:36 -0700

    Add dynamic production level display

--- frontend/index.js
+++ frontend/index.js
@@ -1,24 +1,7 @@
-/**
- * @license
- * Copyright 2019 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
 import * as blazeface from '@tensorflow-models/blazeface';
 import * as tf from '@tensorflow/tfjs';
 import * as tfjsWasm from '@tensorflow/tfjs-backend-wasm';
-import {displayPrediction} from './ui'
+import {displayPrediction, updateChart} from './ui'
 // import * as posenet from  '@tensorflow-models/posenet';
 
 import {extractFaceFeature, extractFeature, loadTruncatedMobileNet, loadCustomModel} from './model';
@@ -66,10 +49,11 @@ const renderPrediction = async () => {
   const predictions = await blazefaceModel.estimateFaces(
     video, returnTensors, flipHorizontal, annotateBoxes);
   const faceFeature = await extractFaceFeature(tf.browser.fromPixels(video), blazefaceModel, truncatedMobileNet);
-  const feature = await extractFeature(tf.browser.fromPixels(video), truncatedMobileNet);
-  // TODO no face.
-  const boredPredictions = await model.predict([faceFeature, feature]);
-  displayPrediction(boredPredictions)
+  if (faceFeature != null){
+    const feature = await extractFeature(tf.browser.fromPixels(video), truncatedMobileNet);
+    const boredPredictions = await model.predict([faceFeature, feature]);
+    await updateChart(boredPredictions);
+  }
 
   if (predictions.length > 0) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
--- frontend/ui.js
+++ frontend/ui.js
@@ -8,3 +8,30 @@ export function displayPrediction(boredPredictions) {
         }
     )
 }
+var chart = new CanvasJS.Chart("chartContainer", {
+	title: {
+		text: "Your Productivity Level"
+	},
+	axisY: {
+        title: "Probability of being productive",
+        maximum: 100,
+		includeZero: true,
+		suffix: " %"
+	},
+	data: [{
+		type: "column",	
+		indexLabel: "{y}",
+		dataPoints: [
+            { label: "Productive Level", y: 50 },
+		]
+	}]
+});
+export async function updateChart(boredPredictions) {
+    boredPredictions.data().then(data => {
+        const prob = data[1].toFixed(4)*100;
+        const newColor = prob <= 30 ? "#FF2500" : prob <= 50 ? "#FF6000" : prob > 50 ? "#6B8E23 " : null;
+        chart.options.data[0].dataPoints = [{label: "Productive Level", y: prob, color: newColor}]
+        chart.render();
+        }
+    )
+};
